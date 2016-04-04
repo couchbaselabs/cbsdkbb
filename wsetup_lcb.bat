109f2f18da5516fe3664ffaa558f8ef0aa1a503d
Brett Lawson <brett19@gmail.com>
2016-04-04 16:17:42 -0700

    Download VC14 versions of libcouchbase.

--- wsetup_lcb.bat
+++ wsetup_lcb.bat
@@ -10,11 +10,13 @@ if "%ARCH%"=="x86" (
   tools\wget --no-check-certificate -nc "http://packages.couchbase.com/clients/c/libcouchbase-%LCBVER%_x86_vc9.zip" -O src\lcb-%LCBVER%-vc9-x86.zip
   tools\wget --no-check-certificate -nc "http://packages.couchbase.com/clients/c/libcouchbase-%LCBVER%_x86_vc10.zip" -O src\lcb-%LCBVER%-vc10-x86.zip
   tools\wget --no-check-certificate -nc "http://packages.couchbase.com/clients/c/libcouchbase-%LCBVER%_x86_vc11.zip" -O src\lcb-%LCBVER%-vc11-x86.zip
+  tools\wget --no-check-certificate -nc "http://packages.couchbase.com/clients/c/libcouchbase-%LCBVER%_x86_vc14.zip" -O src\lcb-%LCBVER%-vc14-x86.zip
 )
 if "%ARCH%"=="x64" (
   tools\wget --no-check-certificate -nc "http://packages.couchbase.com/clients/c/libcouchbase-%LCBVER%_amd64_vc9.zip" -O src\lcb-%LCBVER%-vc9-x64.zip
   tools\wget --no-check-certificate -nc "http://packages.couchbase.com/clients/c/libcouchbase-%LCBVER%_amd64_vc10.zip" -O src\lcb-%LCBVER%-vc10-x64.zip
   tools\wget --no-check-certificate -nc "http://packages.couchbase.com/clients/c/libcouchbase-%LCBVER%_amd64_vc11.zip" -O src\lcb-%LCBVER%-vc11-x64.zip
+  tools\wget --no-check-certificate -nc "http://packages.couchbase.com/clients/c/libcouchbase-%LCBVER%_amd64_vc14.zip" -O src\lcb-%LCBVER%-vc14-x64.zip
 )
 
 
@@ -33,6 +35,10 @@ if "%ARCH%"=="x86" (
     tools\7za x -y -otmp\ src\lcb-%LCBVER%-vc11-x86.zip
     move /Y tmp\libcouchbase-%LCBVER%_x86_vc11 lcb-files\%LCBVER%-vc11-x86
   )
+  if not exist lcb-files\%LCBVER%-vc14-x86 (
+    tools\7za x -y -otmp\ src\lcb-%LCBVER%-vc14-x86.zip
+    move /Y tmp\libcouchbase-%LCBVER%_x86_vc14 lcb-files\%LCBVER%-vc14-x86
+  )
 )
 if "%ARCH%"=="x64" (
   if not exist lcb-files\%LCBVER%-vc9-x64 (
@@ -46,6 +52,10 @@ if "%ARCH%"=="x64" (
   if not exist lcb-files\%LCBVER%-vc11-x64 (
     tools\7za x -y -otmp\ src\lcb-%LCBVER%-vc11-x64.zip
     move /Y tmp\libcouchbase-%LCBVER%_amd64_vc11 lcb-files\%LCBVER%-vc11-x64
+  )
+  if not exist lcb-files\%LCBVER%-vc14-x64 (
+    tools\7za x -y -otmp\ src\lcb-%LCBVER%-vc14-x64.zip
+    move /Y tmp\libcouchbase-%LCBVER%_amd64_vc14 lcb-files\%LCBVER%-vc14-x64
   )
 )
 
