4137d26c674373de089e8c2aafa5c6ba7d68d06b
Sergey Avseyev <sergey.avseyev@gmail.com>
2016-08-03 19:01:19 +0300

    update versions for windows too

--- wsetup_all.bat
+++ wsetup_all.bat
@@ -4,7 +4,7 @@ SET ARCH=%1
 
 echo Installing libcouchbase %ARCH%
 IF "%LCBVER%"=="" (
-    SET LCBVER=2.5.3
+    SET LCBVER=2.6.2
 )
 
 CALL wsetup_lcb.bat %LCBVER% %ARCH%
@@ -17,8 +17,8 @@ CALL wsetup_njs.bat njs 5.6.0 %ARCH%
 
 echo Installing PHP %ARCH%
 CALL wsetup_php.bat 5.4 5.4.45 vc9 %ARCH%
-CALL wsetup_php.bat 5.5 5.5.35 vc11 %ARCH%
-CALL wsetup_php.bat 5.6 5.6.21 vc11 %ARCH%
-CALL wsetup_php.bat 7.0 7.0.7 vc14 %ARCH%
+CALL wsetup_php.bat 5.5 5.5.38 vc11 %ARCH%
+CALL wsetup_php.bat 5.6 5.6.24 vc11 %ARCH%
+CALL wsetup_php.bat 7.0 7.0.9 vc14 %ARCH%
 
 ENDLOCAL
