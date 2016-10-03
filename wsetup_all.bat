9ca603b3d4a8b34b33b8a67518fb105ac92dc7a0
Sergey Avseyev <sergey.avseyev@gmail.com>
2016-10-03 13:42:27 +0300

    update versions

--- wsetup_all.bat
+++ wsetup_all.bat
@@ -4,7 +4,7 @@ SET ARCH=%1
 
 echo Installing libcouchbase %ARCH%
 IF "%LCBVER%"=="" (
-    SET LCBVER=2.6.2
+    SET LCBVER=2.6.3
 )
 
 CALL wsetup_lcb.bat %LCBVER% %ARCH%
@@ -18,7 +18,7 @@ CALL wsetup_njs.bat njs 5.6.0 %ARCH%
 echo Installing PHP %ARCH%
 CALL wsetup_php.bat 5.4 5.4.45 vc9 %ARCH%
 CALL wsetup_php.bat 5.5 5.5.38 vc11 %ARCH%
-CALL wsetup_php.bat 5.6 5.6.24 vc11 %ARCH%
-CALL wsetup_php.bat 7.0 7.0.9 vc14 %ARCH%
+CALL wsetup_php.bat 5.6 5.6.26 vc11 %ARCH%
+CALL wsetup_php.bat 7.0 7.0.11 vc14 %ARCH%
 
 ENDLOCAL
