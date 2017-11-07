12290918cbde774784dd765c5b64605e4885ba09
Brett Lawson <brett19@gmail.com>
2017-11-06 23:50:16 -0800

    Update wsetup_all.bat

--- wsetup_all.bat
+++ wsetup_all.bat
@@ -16,6 +16,7 @@ CALL wsetup_njs.bat njs 4.6.1 %ARCH%
 CALL wsetup_njs.bat njs 5.12.0 %ARCH%
 CALL wsetup_njs.bat njs 6.9.4 %ARCH%
 CALL wsetup_njs.bat njs 7.4.0 %ARCH%
+CALL wsetup_njs.bat njs 8.9.0 %ARCH%
 
 echo Installing PHP %ARCH%
 CALL wsetup_php.bat 5.6 5.6.31 vc11 %ARCH%
